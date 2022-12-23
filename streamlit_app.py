bacb488e1aa695fb7ee1ce6835232d35f23afad0
Duff Warren <95295297+DuffWarren@users.noreply.github.com>
2022-12-23 16:03:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,13 +18,15 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Fruit Picker
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-# Pick list to pick fruit desired
-Fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-Fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(my_fruit_list)
 
+
+# Pick list to pick fruit desired
+#Fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+#Fruits_to_show = my_fruit_list.loc[fruits_selected]
+
 #display the table in the frame
-streamlit.dataframe(fruits_to_show)
+#streamlit.dataframe(fruits_to_show)
 
 
