555f17717dc63df60c27b8cd160fc5da0ae85d50
Duff Warren <95295297+DuffWarren@users.noreply.github.com>
2022-12-29 10:24:05 -0500

    Update streamlit_app.py
    try if not else except added to 
    Fruityvice Fruit Advice!

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,12 +22,6 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 #Fruit column became the UID for the table
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-#Fruit Picker
-#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-
-#streamlit.dataframe(my_fruit_list)
-
-
 # Pick list to pick fruit desired
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
@@ -36,19 +30,18 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 # streamlit.text(fruityvice_response)
 
-streamlit.header("Fruityvice Fruit Advice!")
 # New section to diplay fruityvice API response
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-# import requests - imported above
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json())
-# normalize the ugly text
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Output screen as a table
-streamlit.dataframe(fruityvice_normalized)
-
+streamlit.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?') # took out 'Kiwi' default
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 # Stop here for the moment to troubleshoot
 streamlit.stop()
